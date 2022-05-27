4fa61a97363710ec87188ebc4e06ced95e01222f
k200481 <k200481@gmail.com>
2022-05-27 23:38:12 +0500

    assignments done

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -3,7 +3,7 @@ import rsa
 
 def dump(device_name, block_size):
     ifil = open(device_name, 'rb')
-    ofil = open(f'{device_name}_key', 'wb')
+    ofil = open(f'{device_name}_img', 'wb')
     bytes = 'temp'
     while bytes:
         bytes = ifil.read(block_size)
@@ -14,7 +14,7 @@ def sign(device_name, sk):
     return rsa.sign(fil, sk, 'SHA-1')
 
 def verify(device_name, pk, sgn):
-    f = open(device_name, 'r')
+    f = open(device_name, 'rb')
     rsa.verify(f, sgn, pk)
     print('verified')
 
@@ -37,12 +37,12 @@ if __name__ == "__main__":
             open('pk.pem', 'wb').write(pk.save_pkcs1())
             open('sk.pem', 'wb').write(sk.save_pkcs1())
             print('new pk and sk generated')
-            sgn = sign(device_name, rsa.PrivateKey.load_pkcs1(open('sk.pem').read()))
+            sgn = sign(device_name, rsa.PrivateKey.load_pkcs1(open('sk.pem', 'rb').read()))
         else:
-            sgn = sign(device_name, rsa.PrivateKey.load_pkcs1(open(args.k).read()))
+            sgn = sign(device_name, rsa.PrivateKey.load_pkcs1(open(args.k, 'rb').read()))
         open('signature', 'wb').write(sgn)
     elif args.option == 'v':
         if args.k and args.s:
-            verify(device_name, rsa.PublicKey.load_pkcs1(open(args.k).read()), open(args.s).read())
+            verify(device_name, rsa.PublicKey.load_pkcs1(open(args.k, 'rb').read()), open(args.s, 'rb').read())
         else:
             print('need a public key and signature to verify')
